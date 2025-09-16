7c9412b3dd4c43c868f02a0940fdac2b7812a455
Rene APEDOH <125088077+Rino000@users.noreply.github.com>
2025-09-16 14:27:53 +0000

    commit rene

--- app/pages/parent/create-enseignant.vue
+++ app/pages/parent/create-enseignant.vue
@@ -32,7 +32,7 @@
               <td class="border px-4 py-2">{{ enseignant.enseignant.courriel }}</td>
               <td class="border px-4 py-2 text-center">
                 <button @click="associer(enseignant.enseignant.id)" 
-                <button @click="associer(enseignant.enseignant.id)" 
+             
                         class="bg-indigo-600 text-white px-3 py-1 rounded hover:bg-indigo-700 transition">
                   Associer
                 </button>
